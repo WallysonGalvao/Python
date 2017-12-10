7d16f1c783be9656e5a18ddb7baa28a28439b4e2
Wallyson Galvão <wallyson.galvao@gmail.com>
2017-12-10 16:39:07 -0200

    Exercício Python #021 - Tocando um MP3

--- CursoEmVideo/3 - TwentyOneToThirty/Challenge021.py
+++ CursoEmVideo/3 - TwentyOneToThirty/Challenge021.py
@@ -1,14 +1,8 @@
-from random import shuffle
-# Challenge 021
-# The same teacher of previous challenge wants to solve the order of presentation of the students' work.
-# Make a program that reads the name of four students and shows the order drawn.
+"""Make a program in Python that opens and plays audio from an MP3 file."""
+import pygame
 print("Challenge 021")
-print("The same teacher of previous challenge wants to solve the order of presentation of the students' work.")
-print("Make a program that reads the name of four students and shows the order drawn.")
-firstStudent = str(input("First student: "))
-secondStudent = str(input("Second student: "))
-thirdStudent = str(input("Third student: "))
-fourthStudent = str(input("Fourth student: "))
-students = [firstStudent, secondStudent, thirdStudent, fourthStudent]
-shuffle(students)
-print("The order of presentation will be: {}".format(students))
+print("Make a program in Python that opens and plays audio from an MP3 file.")
+pygame.init()
+pygame.mixer.music.load('Challenge021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
