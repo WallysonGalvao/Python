85512cf34f23d55e0dbb74ca70717168e3f2cc4d
Wallyson Galvão <wallyson.galvao@gmail.com>
2017-12-10 17:02:00 -0200

    Exercício Python #022 - Analisador de Textos

---
+++ CursoEmVideo/3 - TwentyOneToThirty/Challenge022.py
@@ -0,0 +1,16 @@
+"""Analyzing Texts"""
+print("Challenge 021")
+print("Create a program that reads a person's full name and shows: ")
+print("> The name with all letters uppercase and lowercase.")
+print("> How many letters in all (without considering spaces).")
+print("> How many letters have the first name.")
+
+yourName = str(input('Type your name: ')).strip()
+firstName = yourName.split()[0]
+
+print('Analyzing yout name...')
+print('Your name in uppercase is {}'.format(yourName.upper()))
+print('Your name in lowercase is {}'.format(yourName.lower()))
+print('Your name have {} letters'.format(len(yourName) - yourName.count(' ')))
+print('Your first name is {} and he have {} letters'.format(
+    firstName, yourName.find(' ')))
