8b5022e888466a010a7e088b57d9ad85956a4d09
Wallyson Galvão <wallyson.galvao@gmail.com>
2017-12-10 16:42:23 -0200

    Exercício Python #001 - Deixando tudo pronto

--- CursoEmVideo/1 - OneToTen/Challenge001.py
+++ CursoEmVideo/1 - OneToTen/Challenge001.py
@@ -1,5 +1,4 @@
-# Challenge 001
-# Create a program that write "Hello, World!" on the screen.
+"""Create a program that write "Hello, World!" on the screen."""
 print("Challenge 001")
 print("Create a program that write \"Hello, World!\" in screen.")
 print("Hello, World")
