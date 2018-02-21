4d0cb3a044cec08462d663e57f5bb3f0d18fc166
Wallyson Cunha <wallyson.cunha@firstdecision.local>
2018-02-21 14:46:50 -0300

    Exercício Python #016 - Quebrando um Número

---
+++ CursoEmVideo/2 - ElevenToTwenty/Challenge016.py
@@ -0,0 +1,9 @@
+"""Breaking a Number"""
+
+# Challenge 016
+# Create a program that read a any number and show on the screen its integer portion.
+print("Challenge 016")
+print("Create a program that read a any number and show on the screen its integer portion.")
+value = float(input("Enter a value: "))
+portion = round(value)
+print("The value typed was {} and its integer portion is {}".format(value, portion))
