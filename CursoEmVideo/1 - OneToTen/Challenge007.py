aa148b403dcb2f7e5220a1a8761bf581a617af9e
Wallyson Cunha <wallyson.cunha@firstdecision.local>
2018-02-21 14:30:48 -0300

    Exercício Python #007 - Média Aritmética

---
+++ CursoEmVideo/1 - OneToTen/Challenge007.py
@@ -0,0 +1,10 @@
+""""Arithmetic Mean"""
+
+# Challenge 007
+# Develop a program that reads a student's two grades, computes and shows your average.
+print("Challenge 007")
+print("Develop a program that reads a student's two grades, computes and shows your average.")
+firstGrade = float(input("First grade of student: "))
+secondGrade = float(input("Second grade of student: "))
+average = (firstGrade + secondGrade) / 2
+print("The average between {:.1f} and {:.1f} is equals of {:.1f}".format(firstGrade, secondGrade, average))
