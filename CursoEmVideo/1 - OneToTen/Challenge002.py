9d3cf093ced9c2d9caa7812bb1488e7626dc9d5b
Wallyson Cunha <wallyson.cunha@firstdecision.local>
2018-02-21 14:15:03 -0300

    Exercício Python #002 - Respondendo ao usuário

---
+++ CursoEmVideo/1 - OneToTen/Challenge002.py
@@ -0,0 +1,8 @@
+""""Responding to user"""
+
+# Challenge 002
+# Make a program that reads the name a person and show a welcome message.
+print("Challenge 002")
+print("Make a program that read the name a person and show a welcome message.")
+userName = input("Write your name: ")
+print("It's a pleasure meet you, {}!".format(userName))
