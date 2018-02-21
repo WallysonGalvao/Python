af50b1904ada62acf1c7bc5933fdf3d6c555d02f
Wallyson Cunha <wallyson.cunha@firstdecision.local>
2018-02-21 14:21:59 -0300

    Exercício Python #003 - Somando dois números

---
+++ CursoEmVideo/1 - OneToTen/Challenge003.py
@@ -0,0 +1,10 @@
+""""Adding two numbers"""
+
+# Challenge 003
+# Create a program that reads two numbers and shows the sum between them.
+print("Challenge 003")
+print("Create a program that reads two numbers and shows the sum between them.")
+number1 = int(input("Write a first number: "))
+number2 = int(input("Write a second number: "))
+resultOfSum = number1 + number2
+print("The sum between {} and {} is equals of {}".format(number1, number2, resultOfSum))
