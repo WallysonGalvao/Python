ae492df5d5abf8f2f5651019e941bdce9dd44ead
Wallyson Cunha <wallyson.cunha@firstdecision.local>
2018-02-21 14:33:09 -0300

    Exercício Python #009 - Tabuada

---
+++ CursoEmVideo/1 - OneToTen/Challenge009.py
@@ -0,0 +1,29 @@
+""""Tabuada"""
+
+# Challenge 009
+# Make a program that read any integer and shows on the screen its table.
+print("Challenge 009")
+print("Make a program that read any integer and shows on the screen its table.")
+number = int(input("Type it a number to see its table: "))
+multipliedByOne = number * 1
+multipliedByTwo = number * 2
+multipliedByThree = number * 3
+multipliedByFour = number * 4
+multipliedByFive = number * 5
+multipliedBySix = number * 6
+multipliedBySeven = number * 7
+multipliedByEight = number * 8
+multipliedByNine = number * 9
+multipliedByTen = number * 10
+print("=============")
+print("{} x {:2} = {}".format(number, 1, multipliedByOne))
+print("{} x {:2} = {}".format(number, 2, multipliedByTwo))
+print("{} x {:2} = {}".format(number, 3, multipliedByThree))
+print("{} x {:2} = {}".format(number, 4, multipliedByFour))
+print("{} x {:2} = {}".format(number, 5, multipliedByFive))
+print("{} x {:2} = {}".format(number, 6, multipliedBySix))
+print("{} x {:2} = {}".format(number, 7, multipliedBySeven))
+print("{} x {:2} = {}".format(number, 8, multipliedByEight))
+print("{} x {:2} = {}".format(number, 9, multipliedByNine))
+print("{} x {:2} = {}".format(number, 10, multipliedByTen))
+print("============="
