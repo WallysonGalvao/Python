f5a51e6d3c04c20778576cfc46a4493a1b1bdbdf
Wallyson Cunha <wallyson.cunha@firstdecision.local>
2018-02-25 20:53:03 -0300

    Django: Programação Web com Python e simplicidade

---
+++ Alura/Django/connectedin/perfis/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from perfis.views import index, exibir, convidar, aceitar
+
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$', exibir, name='exibir'),
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', convidar, name='convidar'),
+    url(r'^convite/(?P<convite_id>\d+)/aceitar$', aceitar, name='aceitar')
+]
--- Alura/README.md
+++ Alura/README.md
@@ -2,8 +2,10 @@
 
 ## Códigos da Carreira Desenvolvedor Python
 
-### Python 3 Parte I: Introdução a nova versão da linguagem
+### Python 3 Parte I: Introdução à nova versão da linguagem
 
 ### Python 3 Parte II: Avançando na linguagem
 
 ### Python 3: Introdução a Orientação a Objetos
+
+### Django: Programação Web com Python e simplicidade
